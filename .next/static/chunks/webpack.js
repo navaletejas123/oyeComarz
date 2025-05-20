8e0f13e073c67fd55e4497067806b4af352344dd
tejas navale <tejas.navale@oyelabs.com>
2025-05-20 12:54:52 +0530

    fix deployment error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e877a01179eb906"; }
+/******/ 		__webpack_require__.h = function() { return "851c4d4cc552717e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
